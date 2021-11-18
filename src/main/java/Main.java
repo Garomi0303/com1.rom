03e8c9b793c14f0a63d80725fa88f174c1d2e330
Garomi0303 <ganich468@gmail.com>
2021-11-18 16:57:54 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Привіт Львів");
+        System.out.println("это не все что я умею, чесно");
+    }
+}
